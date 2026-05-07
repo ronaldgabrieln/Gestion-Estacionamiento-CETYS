d60921ed2ddfa0e44362695d1cdc966bc5894778
Ronald Narain <ronaldgabrieln@gmail.com>
2026-05-07 13:18:19 -0700

    registrar vehiculo

--- administrarSerializacion.java
+++ administrarSerializacion.java
@@ -1,9 +1,10 @@
+package oop.estacionamientogestion;
 import java.util.ArrayList;
 import java.io.*;
 public class administrarSerializacion {
 
     //Guardar vehiculo dentro de un ArrayList
-    public static void guardarObjetos(ArrayList</* clase vehiculo */> lista, String nombreArchivo) {
+    public static void guardarObjetos(ArrayList<Vehiculo> lista, String nombreArchivo) {
         try (ObjectOutputStream escritorObjetos = new ObjectOutputStream(new FileOutputStream(nombreArchivo))) {
 
             escritorObjetos.writeObject(lista);
@@ -13,12 +14,21 @@ public class administrarSerializacion {
             System.err.println("Error al guardar la lista: " + e.getMessage());
         }
     }
+    public static void guardarObjetosVisitas(ArrayList<Visita> lista, String nombreArchivo) {
+        try (ObjectOutputStream escritorObjetos = new ObjectOutputStream(new FileOutputStream(nombreArchivo))) {
 
+            escritorObjetos.writeObject(lista);
+            System.out.println("la lista de Visitas fue escrito en: " + nombreArchivo);
+
+        } catch (IOException e) {
+            System.err.println("Error al guardar la lista: " + e.getMessage());
+        }
+    }
     //Lee el ArrayList de Vehiculo
-    public static Arraylist</* clase vehiculo */> leerObjetos(String nombreArchivo) {
+    public static ArrayList<Vehiculo> leerObjetos(String nombreArchivo) {
         try (ObjectInputStream lectorObjetos = new ObjectInputStream(new FileInputStream(nombreArchivo))) {
 
-            Arraylist</* clase vehiculo */> listaRecuperada = (ArrayList</* clase vehiculo */>) lectorObjetos.readObject();
+            ArrayList<Vehiculo> listaRecuperada = (ArrayList<Vehiculo>) lectorObjetos.readObject();
             System.out.println("La lista de " + listaRecuperada.size() + "vehiculos recuperada de " + nombreArchivo);
             return listaRecuperada;
 
