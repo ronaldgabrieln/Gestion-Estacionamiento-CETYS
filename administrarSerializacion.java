d899880738890b0bec39412b142e00e17fe6694d
Nicole <nicole.riverafelix@gmail.com>
2026-05-07 13:16:50 -0700

    se agrego la Serializacion y Deserializacion para los Vehiculos y sus visitas/historial

--- administrarSerializacion.java
+++ administrarSerializacion.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class administrarSerializacion {
 
     //Guardar vehiculo dentro de un ArrayList
-    public static void guardarObjetos(ArrayList</* clase vehiculo */> lista, String nombreArchivo) {
+    public static void guardarObjetosVehiculo(ArrayList</* clase vehiculo */> lista, String nombreArchivo) {
         try (ObjectOutputStream escritorObjetos = new ObjectOutputStream(new FileOutputStream(nombreArchivo))) {
 
             escritorObjetos.writeObject(lista);
@@ -15,11 +15,11 @@ public class administrarSerializacion {
     }
 
     //Lee el ArrayList de Vehiculo
-    public static Arraylist</* clase vehiculo */> leerObjetos(String nombreArchivo) {
+    public static Arraylist</* clase vehiculo */> leerObjetosVehiculo(String nombreArchivo) {
         try (ObjectInputStream lectorObjetos = new ObjectInputStream(new FileInputStream(nombreArchivo))) {
 
             Arraylist</* clase vehiculo */> listaRecuperada = (ArrayList</* clase vehiculo */>) lectorObjetos.readObject();
-            System.out.println("La lista de " + listaRecuperada.size() + "vehiculos recuperada de " + nombreArchivo);
+            System.out.println("La lista de " + listaRecuperada.size() + "vehiculos fue recuperada de " + nombreArchivo);
             return listaRecuperada;
 
         } catch (IOException e){
@@ -30,4 +30,38 @@ public class administrarSerializacion {
         }
         return null;
     }
+
+    //Guardar visitas dentro de un ArrayList
+    public static void guardarObjetosVisita(ArrayList</* clase visita */> lista, String nombreArchivo) {
+        try (ObjectOutputStream escritorObjetosVisita = new ObjectOutputStream(new FileOutputStream(nombreArchivo))) {
+
+            escritorObjetos.writeObject(lista);
+            System.out.println("la lista de Vehiculos fue escrito en: " + nombreArchivo);
+
+        } catch (IOException e) {
+            System.err.println("Error al guardar la lista: " + e.getMessage());
+        }
+    }
+
+    //Lee el ArrayList de visita/historial
+    public static Arraylist</* clase visita */> leerObjetosVisita(String nombreArchivo) {
+        try (ObjectInputStream lectorObjetos = new ObjectInputStream(new FileInputStream(nombreArchivo))) {
+
+            Arraylist</* clase visita */> listaRecuperadaVisita = (ArrayList</* clase visita */>) lectorObjetos.readObject();
+            System.out.println("El historial con " + listaRecuperadaVisita.size() + " vehculos fue recuperada en " + nombreArchivo);
+            return listaRecuperadaVisita;
+
+        } catch (IOException e){
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("La clase del objeto no fue encontrado: " + e.getMessage());
+        }
+        return null;
+    }
+
+
+
+
+
 }
